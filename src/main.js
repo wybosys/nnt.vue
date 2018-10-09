859c2b0d590a91fe8deb99a23928c93387f3c76e
wybosys <wybosys@gmail.com>
2018-10-09 16:57:56 +0800

    config

--- src/main.js
+++ src/main.js
@@ -4,9 +4,41 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import App from './App'
 import router from './router'
+import VueConfigManager from 'vue-config-manager'
 
 Vue.config.productionTip = false
 Vue.use(Vuex)
+Vue.use(VueConfigManager, {
+  defaults: {
+    LOCAL: false,
+    DEVOPS: false,
+    DEVOPS_DEVELOP: false,
+    DEVOPS_RELEASE: false,
+    DEBUG: false,
+    DEVELOP: false,
+    PUBLISH: false,
+    DISTRIBUTION: false
+  },
+  hosts: {
+    'localhost': {
+      LOCAL: true,
+      DEBUG: true,
+      DEVELOP: true
+    },
+    '91egame.com': {
+      DEVOPS: true,
+      DEVOPS_DEVELOP: true,
+      PUBLISH: true,
+      DISTRIBUTION: true
+    },
+    '91yigame.com': {
+      DEVOPS: true,
+      DEVOPS_RELEASE: true,
+      PUBLISH: true,
+      DISTRIBUTION: true
+    }
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
