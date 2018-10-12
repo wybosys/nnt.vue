928922e614f91f77bdbd696793a53d764fb7b999
wybosys <wybosys@gmail.com>
2018-10-12 17:41:18 +0800

    echo

--- src/app/apis.ts
+++ src/app/apis.ts
@@ -1,102 +1,94 @@
 import {Model} from "./model-impl";
 
-namespace models {
 
 
 
 
 
-
-    export class Echoo extends Model {
-    
-        @Model.string(1, [Model.input], "输入")
-        input:string;
-    
-        @Model.string(2, [Model.output], "输出")
-        output:string;
-    
-        @Model.integer(3, [Model.output], "服务器时间")
-        time:number;
-    
-        @Model.json(4, [Model.output])
-        json:Object;
-    
-        @Model.map(5, Model.string_t, Model.integer_t, [Model.output])
-        map:Map<string, number>;
-    
-        @Model.array(6, Model.double_t, [Model.output])
-        array:Array<number>;
-    
-    }
-
-    export class Login extends Model {
-    
-        @Model.string(1, [Model.input], "随便输入一个用户id")
-        uid:string;
-    
-        @Model.string(2, [Model.output])
-        sid:string;
-    
-    }
-
-    export class User extends Model {
-    
-        @Model.string(1, [Model.output], "当前用户id")
-        uid:string;
-    
-    }
-
-    export class Message extends Model {
-    
-        @Model.string(1, [Model.output], "消息体")
-        content:string;
-    
-    }
-
-    export class Upload extends Model {
-    
-        @Model.file(1, [Model.input, Model.output], "选择一个图片")
-        file:any;
-    
-    }
-
+export class Echoo extends Model {
+  
+      @Model.string(1, [Model.input], "输入")
+      input:string;
+  
+      @Model.string(2, [Model.output], "输出")
+      output:string;
+  
+      @Model.integer(3, [Model.output], "服务器时间")
+      time:number;
+  
+      @Model.json(4, [Model.output])
+      json:Object;
+  
+      @Model.map(5, Model.string_t, Model.integer_t, [Model.output])
+      map:Map<string, number>;
+  
+      @Model.array(6, Model.double_t, [Model.output])
+      array:Array<number>;
+  
 }
 
-namespace routers {
-
-    export let SampleEcho = ["sample.echo", models.Echoo, ""];
-
-    export let SampleLogin = ["sample.login", models.Login, ""];
-
-    export let SampleUser = ["sample.user", models.User, ""];
-
-    export let SampleMessage = ["sample.message", models.Message, "监听消息炸弹"];
-
-    export let SampleUpload = ["sample.upload", models.Upload, "上传图片"];
-
+export class Login extends Model {
+  
+      @Model.string(1, [Model.input], "随便输入一个用户id")
+      uid:string;
+  
+      @Model.string(2, [Model.output])
+      sid:string;
+  
 }
 
-namespace api {
+export class User extends Model {
+  
+      @Model.string(1, [Model.output], "当前用户id")
+      uid:string;
+  
+}
 
-    export function SampleEcho():models.Echoo {
-    return Model.NewRequest(routers.SampleEcho);
-    }
+export class Message extends Model {
+  
+      @Model.string(1, [Model.output], "消息体")
+      content:string;
+  
+}
 
-    export function SampleLogin():models.Login {
-    return Model.NewRequest(routers.SampleLogin);
-    }
+export class Upload extends Model {
+  
+      @Model.file(1, [Model.input, Model.output], "选择一个图片")
+      file:any;
+  
+}
 
-    export function SampleUser():models.User {
-    return Model.NewRequest(routers.SampleUser);
-    }
 
-    export function SampleMessage():models.Message {
-    return Model.NewRequest(routers.SampleMessage);
-    }
 
-    export function SampleUpload():models.Upload {
-    return Model.NewRequest(routers.SampleUpload);
-    }
+export let RSampleEcho = ["sample.echo", Echoo, ""];
 
+export let RSampleLogin = ["sample.login", Login, ""];
+
+export let RSampleUser = ["sample.user", User, ""];
+
+export let RSampleMessage = ["sample.message", Message, "监听消息炸弹"];
+
+export let RSampleUpload = ["sample.upload", Upload, "上传图片"];
+
+
+
+export function SampleEcho():Echoo {
+  return Model.NewRequest(RSampleEcho);
+}
+
+export function SampleLogin():Login {
+  return Model.NewRequest(RSampleLogin);
+}
+
+export function SampleUser():User {
+  return Model.NewRequest(RSampleUser);
+}
+
+export function SampleMessage():Message {
+  return Model.NewRequest(RSampleMessage);
+}
+
+export function SampleUpload():Upload {
+  return Model.NewRequest(RSampleUpload);
 }
 
--- src/components/Echo.vue
+++ src/components/Echo.vue
@@ -1,14 +1,32 @@
 <template>
   <div>
-    <input/>
-    <button>echo</button>
-    <label></label>
+    <input v-model="input"></input>
+    <button v-on:click="actEcho">echo</button>
+    <label>{{output}}</label>
   </div>
 </template>
 
 <script lang="ts">
+import {RestSession} from "../nnt/RestSession";
+import {SampleEcho} from "../app/apis.ts";
+
 export default {
-  name: "Echo"
+  name: "Echo",
+  data() {
+    return {
+      input: '',
+      output: ''
+    }
+  },
+  methods: {
+    actEcho() {
+      let m = SampleEcho()
+      m.input = this.input
+      RestSession.fetch(m, () => {
+        this.output = m.output
+      })
+    }
+  }
 }
 </script>
 
