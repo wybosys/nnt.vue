e75a4ff94c991e88c5c0ae8129ab3da7381e0908
wybosys <wybosys@gmail.com>
2019-01-15 15:56:14 +0800

    prj

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/framework/nnt.vue/dist/',
+    assetsPublicPath: '/framework/nnt.vue',
 
     /**
      * Source Maps
