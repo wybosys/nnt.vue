3a1b3e4882b08f65c7398ba91d8c5158a1ed1d54
wybosys <wybosys@gmail.com>
2019-01-15 14:16:12 +0800

    router

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,13 +3,10 @@ import Router from 'vue-router'
 
 const Sample = () => import("../components/Sample.vue")
 
-Vue.use(Router)
-
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Sample',
       component: Sample
     }
   ]
