14f8418e985b881da22715564e7a7cf245887071
wybosys <wybosys@gmail.com>
2019-01-15 15:36:05 +0800

    prj

--- build/tools.js
+++ build/tools.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 
 function StopDevServer() {
   if (fs.existsSync('run/dev-server.pid')) {
@@ -20,7 +21,63 @@ function SaveDevServer() {
 
 function GenRoutes() {
   // 默认输出到src/router/index.ts中
+  // 默认组件保存在src/components中
 
+  // { path: filepath }
+  let routes = {}
+
+  // 列出所有目录中的组件
+  ListRoutesInDirectory('src/components', '', routes)
+
+  let imports = []
+  let defs = []
+
+  for (let key in routes) {
+    let name = key.replace(/\//g, '_')
+    imports.push('const ' + name + ' = () => import("../components' + routes[key] + '")')
+    defs.push("\t{\n\t\tpath: '" + key + "',\n\t\tcomponent: " + name + "\n\t}")
+  }
+
+  content = imports.join('\n')
+  content += '\n'
+  content += 'export default [\n'
+  content += defs.join(',\n')
+  content += '\n]\n'
+
+  // 保存
+  fs.writeFileSync('src/router/routes.ts', content)
+}
+
+function UppercaseFirst(str) {
+  if (!str || str.length == 0)
+    return str
+  return str[0].toUpperCase() + str.substr(1)
+}
+
+function ListRoutesInDirectory(dir, cur, result) {
+  let cfg = dir + '/config.json'
+  if (fs.existsSync(cfg)) {
+    let cfgobj = JSON.parse(fs.readFileSync(cfg))
+    let rootname = UppercaseFirst(path.basename(cur))
+    if (fs.existsSync(dir + '/' + rootname + '.vue')) {
+      result[cur] = cur + '/' + rootname + '.vue'
+      if (cfgobj.default) {
+        result[path.dirname(cur)] = cur + '/' + rootname + '.vue'
+      }
+    }
+  }
+
+  fs.readdirSync(dir).forEach(each => {
+    let st = fs.statSync(dir + '/' + each)
+    if (st.isDirectory()) {
+      ListRoutesInDirectory(dir + '/' + each, cur + '/' + each, result)
+    } else {
+      if (path.extname(each) == ".vue") {
+        let name = path.basename(each, ".vue").toLowerCase()
+        result[cur + '/' + name] = cur + '/' + each
+      }
+    }
+  })
 }
 
 if (process.argv.indexOf('stop') != -1) {
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,8 +1,32 @@
-const Sample = () => import("../components/sample/Sample.vue")
-
+const _sample = () => import("../components/sample/Sample.vue")
+const _ = () => import("../components/sample/Sample.vue")
+const _sample_audiorecorder = () => import("../components/sample/AudioRecorder.vue")
+const _sample_echo = () => import("../components/sample/Echo.vue")
+const _sample_helloworld = () => import("../components/sample/HelloWorld.vue")
+const _sample_sample = () => import("../components/sample/Sample.vue")
 export default [
-  {
-    path: '/',
-    component: Sample
-  }
+	{
+		path: '/sample',
+		component: _sample
+	},
+	{
+		path: '/',
+		component: _
+	},
+	{
+		path: '/sample/audiorecorder',
+		component: _sample_audiorecorder
+	},
+	{
+		path: '/sample/echo',
+		component: _sample_echo
+	},
+	{
+		path: '/sample/helloworld',
+		component: _sample_helloworld
+	},
+	{
+		path: '/sample/sample',
+		component: _sample_sample
+	}
 ]
