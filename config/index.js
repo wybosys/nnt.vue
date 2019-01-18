c18a9e3cee7e7c1a015885102056bd8cd253d1a8
wybosys <wybosys@gmail.com>
2019-01-18 15:24:36 +0800

    build

--- config/index.js
+++ config/index.js
@@ -44,7 +44,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: tools.GetDevopsDomain(),
+    assetsPublicPath: tools.GetDevopsDomain() + '/',
 
     /**
      * Source Maps
